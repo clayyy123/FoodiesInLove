fa2b0f28aa4a893148f13ff59b16dd0e87ba849b
Clayton Wang <claydwang@gmail.com>
2019-04-11 00:04:39 -0700

    css redo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,101 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import httpClient from './httpClient'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import httpClient from './httpClient';
 
-import NavBar from './NavBar'
-import LogIn from './views/LogIn'
-import LogOut from './views/LogOut'
-import SignUp from './views/SignUp'
-import Home from './views/Home'
-import Feed from "./views/Feed"
-import Profile from "./views/Profile"
-import Matches from "./views/Matches"
-import Chat from "./views/Chat"
+import NavBar from './NavBar';
+import LogIn from './views/LogIn';
+import LogOut from './views/LogOut';
+import SignUp from './views/SignUp';
+import Home from './views/Home';
+import Feed from './views/Feed';
+import Profile from './views/Profile';
+import Matches from './views/Matches';
+import Chat from './views/Chat';
 
 class App extends React.Component {
-	state = { currentUser: httpClient.getCurrentUser() }
+  state = { currentUser: httpClient.getCurrentUser() };
 
-	onLoginSuccess(user) {
-		this.setState({ currentUser: httpClient.getCurrentUser() })
-	}
+  onLoginSuccess(user) {
+    this.setState({ currentUser: httpClient.getCurrentUser() });
+  }
 
-	logOut() {
-		httpClient.logOut()
-		this.setState({ currentUser: null })
-	}
-	
-	render() {
-		const { currentUser } = this.state
-		return (
-			<div className='App container'>
+  logOut() {
+    httpClient.logOut();
+    this.setState({ currentUser: null });
+  }
 
-				<NavBar currentUser={currentUser} />
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <div className="wrapper">
+        <NavBar currentUser={currentUser} />
 
-				<Switch>
+        <Switch>
+          <Route
+            path="/matches/:id"
+            render={props => {
+              return <Chat current={currentUser} {...props} />;
+            }}
+          />
 
-					<Route path="/matches/:id" render={(props)=>{
-						return <Chat current={currentUser} {...props}/>
-					}}/>
+          <Route
+            path="/login"
+            render={props => {
+              return (
+                <LogIn
+                  {...props}
+                  onLoginSuccess={this.onLoginSuccess.bind(this)}
+                />
+              );
+            }}
+          />
 
-					<Route path="/login" render={(props) => {
-						return <LogIn {...props} onLoginSuccess={this.onLoginSuccess.bind(this)} />
-					}} />
+          <Route
+            path="/logout"
+            render={props => {
+              return <LogOut onLogOut={this.logOut.bind(this)} />;
+            }}
+          />
 
-					<Route path="/logout" render={(props) => {
-						return <LogOut onLogOut={this.logOut.bind(this)} />
-					}} />
+          {/* the sign up component takes an 'onSignUpSuccess' prop which will perform the same thing as onLoginSuccess: set the state to contain the currentUser */}
+          <Route
+            path="/signup"
+            render={props => {
+              return (
+                <SignUp
+                  {...props}
+                  onSignUpSuccess={this.onLoginSuccess.bind(this)}
+                />
+              );
+            }}
+          />
 
-					{/* the sign up component takes an 'onSignUpSuccess' prop which will perform the same thing as onLoginSuccess: set the state to contain the currentUser */}
-					<Route path="/signup" render={(props) => {
-						return <SignUp {...props} onSignUpSuccess={this.onLoginSuccess.bind(this)} />
-					}} />
+          <Route
+            path="/feed"
+            render={props => {
+              return <Feed {...props} current={currentUser} />;
+            }}
+          />
 
-					<Route path="/feed" render={(props)=>{
-						return <Feed {...props} current={currentUser}/>
-					}}/>
+          <Route
+            path="/profile"
+            render={props => {
+              return <Profile {...props} current={currentUser} />;
+            }}
+          />
 
-					<Route path="/profile" render={(props)=>{
-						return <Profile {...props} current={currentUser}/>
-					}}/>
+          <Route
+            path="/matches"
+            render={routeProps => {
+              return <Matches current={currentUser} />;
+            }}
+          />
 
-					<Route path="/matches" render={(routeProps)=>{
-						return <Matches current={currentUser}/>
-					}}/>
-
-
-					<Route path="/" component={Home} />
-
-				</Switch>
-			</div>
-		)
-	}
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,68 +1,74 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import httpClient from "./httpClient"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import httpClient from './httpClient';
 
 class NavBar extends React.Component {
+  state = {
+    matches: [],
+    show: false
+  };
 
-	state={
-		matches:[],
-		show: false
-	}
+  handleShow() {
+    httpClient.getMatches(this.props.currentUser._id).then(serverResponse => {
+      this.setState({
+        matches: serverResponse.data,
+        show: !this.state.show
+      });
+    });
+  }
 
-handleShow(){
-	httpClient.getMatches(this.props.currentUser._id).then((serverResponse)=>{
-	this.setState({
-		matches: serverResponse.data,
-		show: !this.state.show
-	})
-})
+  handleNoShow() {
+    this.setState({
+      show: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="nav">
+        <div className="horizontal">
+          <div className="logo">
+            <Link to="/">FoodiesInLove</Link>
+          </div>
+          <div className="links">
+            {this.props.currentUser ? (
+              <span className="span-container">
+                <Link className="start-matching" to="/feed">
+                  Eat!
+                </Link>
+                <span onClick={this.handleShow.bind(this)}>
+                  {this.props.currentUser.name}
+                </span>
+                {this.state.show ? (
+                  <span>
+                    <Link className="right-flex" to="/matches">
+                      Matches<span>({this.state.matches.length})</span>
+                    </Link>
+                    <Link className="right-flex" to="/profile">
+                      Profile
+                    </Link>
+                    <Link
+                      className="right-flex"
+                      to="/logout"
+                      onClick={this.handleNoShow.bind(this)}
+                    >
+                      Log Out
+                    </Link>
+                  </span>
+                ) : (
+                  <span />
+                )}
+              </span>
+            ) : (
+              <span className="horizontal">
+                <Link to="/login">Log In</Link>
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-handleNoShow(){
-	this.setState({
-		show:false
-	})
-}
-
-render(){
-	return (
-		<div className='NavBar'>
-			<div className="horizontal">
-			<Link id="logo" to="/">FoodiesInLove</Link>
-			{this.props.currentUser
-				? (
-					<span >
-						<Link id="start-matching" to="/feed"> Start Matching </Link>
-						<span onClick={this.handleShow.bind(this)}>
-						<Link className="right-flex" id="dropdown" to="#">{this.props.currentUser.name}</Link>
-						</span>
-						{this.state.show
-					? (
-						<span className="horizontal">
-						
-						<Link className="right-flex" to="/matches">Matches<span>({this.state.matches.length})</span></Link>
-						<Link className="right-flex" to="/profile">Profile</Link>
-						<Link className="right-flex" to="/logout" onClick={this.handleNoShow.bind(this)}>Log Out</Link>			
-							
-						</span>
-						)
-					: (
-						<span></span>
-						)
-					}
-					
-					</span>
-				)
-				: (
-					<span className="horizontal">
-						<Link to="/login">Log In</Link>
-					</span>
-				)
-			}
-			</div>
-		</div>
-	)
-}
-}
-
-export default NavBar
+export default NavBar;
